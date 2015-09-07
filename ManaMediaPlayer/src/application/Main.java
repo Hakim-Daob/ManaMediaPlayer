95796da7d056b907ff43f399f147fc4edd0bed17
Hakim Aldaoub <hake_daob@yahoo.com>
2015-09-07 09:02:56 -0400

    opening a new file menu is working.  Mana Media Player is funictioning perfectly and smoothly! some Icons need to be replaced, and some other features need to be added.

--- ManaMediaPlayer/src/application/Main.java
+++ ManaMediaPlayer/src/application/Main.java
@@ -1,16 +1,54 @@
 package application;
 	
+
+import java.io.File;
+import java.net.MalformedURLException;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		Player player = new Player("file:///d:Life.mp4");
+	
+	Player player;
+	FileChooser fileChosser;
+	public void start(final Stage primaryStage) {
+		
+		MenuItem openFile = new MenuItem("openFile");
+		Menu fileMenu=new Menu("File");
+		MenuBar menuBar = new MenuBar();
+		
+		fileMenu.getItems().add(openFile);
+		menuBar.getMenus().add(fileMenu);
+		
+		fileChosser= new FileChooser();
+		openFile.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				File file = fileChosser.showOpenDialog(primaryStage);
+				if (file!=null)
+					try {
+						player=new Player(file.toURI().toURL().toExternalForm());
+						Scene scene = new Scene(player, 720, 535, Color.BLACK);
+						primaryStage.setScene(scene);
+					} catch (MalformedURLException el){
+						el.printStackTrace();
+					}			
+			}
+		});
+	
+		player = new Player("file:///d:Life.mp4");
+		player.setTop(menuBar);
 		Scene scene= new Scene(player,1080, 480, Color.BLACK);
 		primaryStage.setScene(scene);
 		primaryStage.show();			
